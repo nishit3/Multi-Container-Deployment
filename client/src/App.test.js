59a8dc7d3c7a3fb003c1f3a353d3c706f5405208
MortiZ <nishitchaudhary71@gmail.com>
2026-04-12 17:47:53 -0400

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+it("It renderes without crashing", () => {});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
